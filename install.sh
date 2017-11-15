6fdc3b3a5a95148373924c07b8ca18463a0841dc
mose <mose@infocast.tech>
2017-11-15 15:52:08 +0800

    add some useful packages to install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install git zsh vim 
+sudo apt-get install git zsh vim xautolock parcellite feh rxvt-unicode-256color
 
 TERM=xterm-256color
 BG="187"
